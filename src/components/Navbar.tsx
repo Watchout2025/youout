c94006a6b0624858753d849db41a829175db7dea
jinwoo <watchoutofficial2006@gmail.com>
2026-04-09 15:13:27 +0530

    style: update header logo with new SVG icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,9 +68,12 @@ export default function Navbar() {
         <button onClick={toggleSidebar} className="p-2 hover:bg-[#272727] rounded-full hidden md:block">
           <Menu className="w-6 h-6 text-white" />
         </button>
-        <Link href="/" className="flex items-center gap-1">
-          <div className="bg-red-600 p-1 rounded-lg">
-            <div className="w-4 h-3 bg-white" style={{ clipPath: "polygon(0% 0%, 100% 50%, 0% 100%)" }}></div>
+        <Link href="/" className="flex items-center gap-1 group">
+          <div className="flex items-center">
+            <svg width="32" height="22" viewBox="0 0 29 20" fill="none" xmlns="http://www.w3.org/2000/svg" className="transition-transform group-active:scale-95">
+              <path d="M14.4848 20C14.4848 20 23.5695 20 25.8229 19.4C27.0917 19.06 28.0459 18.08 28.3808 16.87C29 14.65 29 9.98 29 9.98C29 9.98 29 5.34 28.3808 3.14C28.0459 1.9 27.0917 0.94 25.8229 0.61C23.5695 0 14.4848 0 14.4848 0C14.4848 0 5.42037 0 3.17711 0.61C1.9286 0.94 0.954148 1.9 0.59888 3.14C0 5.34 0 9.98 0 9.98C0 9.98 0 14.65 0.59888 16.87C0.954148 18.08 1.9286 19.06 3.17711 19.4C5.42037 20 14.4848 20 14.4848 20Z" fill="#FF0033"></path>
+              <path d="M19 10L11.5 5.75V14.25L19 10Z" fill="white"></path>
+            </svg>
           </div>
           <span className="font-bold text-xl tracking-tighter text-white">YouOut</span>
         </Link>
